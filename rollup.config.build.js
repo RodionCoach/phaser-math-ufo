b6f6789bc3fe1d2b6c1476f306844cb6d4707321
Rodion <www.rodionkochetkow@gmail.com>
2021-04-05 12:22:32 +0300

    fix: rollup

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,32 +1,22 @@
 import commonjs from "rollup-plugin-commonjs";
-import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import copy from "rollup-plugin-copy-assets";
+import pkg from "./package.json";
 
 export default {
   input: ["./src/index.ts"],
 
   output: {
-    file: "./build/index.js",
-    name: "math",
-    format: "iife",
+    file: pkg.main,
+    name: "math-ufo",
+    format: "es",
     sourcemap: false,
     intro: "var global = window;",
   },
 
   plugins: [
-    replace({
-      "typeof CANVAS_RENDERER": JSON.stringify(true),
-      "typeof WEBGL_RENDERER": JSON.stringify(true),
-      "typeof EXPERIMENTAL": JSON.stringify(true),
-      "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
-      "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
-      "typeof FEATURE_SOUND": JSON.stringify(true),
-      preventAssignment: true,
-    }),
-
     typescript(),
 
     resolve({
@@ -35,7 +25,6 @@ export default {
 
     commonjs({
       include: ["node_modules/eventemitter3/**", "node_modules/phaser/**", "node_modules/**"],
-      exclude: ["node_modules/phaser/src/polyfills/requestAnimationFrame.js"],
       sourceMap: false,
       ignoreGlobal: true,
     }),
--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,34 +1,23 @@
 import commonjs from "rollup-plugin-commonjs";
-import replace from "@rollup/plugin-replace";
 import serve from "rollup-plugin-serve";
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import copy from "rollup-plugin-copy-assets";
 import livereload from "rollup-plugin-livereload";
 import path from "path";
+import pkg from "./package.json";
 
 export default {
   input: ["./src/index.ts"],
 
   output: {
-    file: "./build/index.js",
-    name: "math",
-    format: "iife",
-    sourcemap: true,
+    file: pkg.main,
+    format: "es",
+    sourcemap: false,
     intro: "var global = window;",
   },
 
   plugins: [
-    replace({
-      "typeof CANVAS_RENDERER": JSON.stringify(true),
-      "typeof WEBGL_RENDERER": JSON.stringify(true),
-      "typeof EXPERIMENTAL": JSON.stringify(true),
-      "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
-      "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
-      "typeof FEATURE_SOUND": JSON.stringify(true),
-      preventAssignment: true,
-    }),
-
     typescript(),
 
     resolve({
@@ -37,7 +26,6 @@ export default {
 
     commonjs({
       include: ["node_modules/eventemitter3/**", "node_modules/phaser/**", "node_modules/**"],
-      exclude: ["node_modules/phaser/src/polyfills/requestAnimationFrame.js"],
       sourceMap: true,
       ignoreGlobal: true,
     }),
